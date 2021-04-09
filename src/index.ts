c11ac692e77fc7d7acdaf4371cc2700f9f533513
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-09 08:19:28 -0400

    chore: add body-parser, cors; update gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
+
 const app = express();
 const PORT = 3000;
+
+app.use(json());
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send("wawawewa!");

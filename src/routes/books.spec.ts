f792e1a20eba1ac40eab9065c084c7774522f2f2
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-13 10:18:00 -0400

    tests: expect root to return all

--- src/routes/books.spec.ts
+++ src/routes/books.spec.ts
@@ -1,11 +1,14 @@
 import supertest from "supertest";
 import { app } from "../app";
+import { db } from "../db";
 
 describe("/api/books", () => {
-  it("GET /", (done) => {
-    const r = supertest(app);
-    const get = r.get("/api/books");
+  it("GET /", async () => {
+    const booksRes = await supertest(app)
+      .get("/api/books")
+      .expect("Content-Type", /json/)
+      .expect(200);
 
-    get.expect("Content-Type", /json/).expect(200, done);
+    expect(booksRes.body).toMatchObject(db);
   });
 });

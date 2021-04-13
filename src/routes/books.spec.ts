0d99a115b85c671c4476e3434d1930b98785313e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-13 17:26:28 -0400

    wip

--- src/routes/books.spec.ts
+++ src/routes/books.spec.ts
@@ -2,24 +2,42 @@ import supertest from "supertest";
 import { app } from "../app";
 import { db } from "../db";
 
-describe("/api/books", () => {
-  it("returns all books on GET /", async () => {
-    const booksRes = await supertest(app)
-      .get("/api/books")
-      .expect("Content-Type", /json/)
-      .expect(200, db);
+/** @name supertest https://github.com/visionmedia/supertest#api
+ * @function .expect(status[,fn]) - Assert response status code.
+ * @function .expect(status,body[,fn]) - Assert response status code and body.
+ * @function .expect(body[,fn]) - Assert response body text with a string, regular expression, or parsed body object.
+ * @function .expect(field,value[,fn]) - Assert header field value with a string or regular expression.
+ * @function .expect(function(res)) - Pass a custom assertion function. It'll be given the response object to check. If the check fails, throw an error
+ */
 
-    expect(booksRes.body).toMatchObject(db);
+describe("/api/books", () => {
+  let request: supertest.SuperTest<supertest.Test>;
+  beforeAll(() => {
+    request = supertest(app);
   });
 
-  it("creates a book on POST /", async () => {
-    const newBook = { title: "Wawawewa" };
+  describe("/", () => {
+    test("GET returns all books", async () => {
+      const booksRes = await request
+        .get("/api/books")
+        .expect("Content-Type", /json/)
+        .expect(200, db);
 
-    const booksRes = await supertest(app)
-      .post("/api/books")
-      .send(newBook)
-      .expect(201);
+      expect(booksRes.body).toMatchObject(db);
+    });
 
-    expect(db).toContainEqual(expect.objectContaining(newBook));
+    test("POST create a new book", async () => {
+      const newBook = { title: "Wawawewa" };
+
+      await request.post("/api/books").send(newBook).expect(201);
+
+      expect(db).toContainEqual(expect.objectContaining(newBook));
+    });
+  });
+
+  describe("/:id", () => {
+    it.todo("GET");
+    it.todo("PUT");
+    it.todo("DELETE");
   });
 });

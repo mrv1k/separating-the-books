0cd82a81b270bc3f659fdca9e04fd9430ea688e7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-13 20:21:31 -0400

    dev: enable logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import express from "express";
 import cors from "cors";
+import winston from "winston";
+import expressWinston from "express-winston";
+import debug from "debug";
 import booksRouter from "./routes/books";
 
 const app = express();
+
+const debugLog = debug("app");
+
+if (process.env.NODE_ENV !== "test") {
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize()
+      ),
+      meta: false,
+    })
+  );
+}
 
 app.use(express.json());
 app.use(cors());
@@ -20,7 +39,7 @@ const server = () => {
 
   const PORT = process.env.PORT || 3000;
   const server = app.listen(PORT, () => {
-    console.log(`Listening on port http://localhost:${PORT}`);
+    debugLog(`\nListening on port http://localhost:${PORT}`);
   });
   return server;
 };

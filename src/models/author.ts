91b30511eb64338d32a7ed089046aed83307575a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-15 12:41:32 -0400

    add author types

--- src/models/author.ts
+++ src/models/author.ts
@@ -1,17 +1,23 @@
-import mongoose, { Schema } from "mongoose";
+import { model, Document, Schema, Model } from "mongoose";
+
+export interface IAuthor extends Document {
+  first_name: string;
+  last_name: string;
+}
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
 });
 
-AuthorSchema.virtual("name").get(function () {
-  console.log(this);
+AuthorSchema.virtual("name").get(function (this: IAuthor) {
   return this.first_name + this.last_name;
 });
 
-AuthorSchema.virtual("url").get(function () {
+AuthorSchema.virtual("url").get(function (this: IAuthor) {
   return "authors/" + this._id;
 });
 
-export default mongoose.model("AuthorSchema", AuthorSchema);
+const Author: Model<IAuthor> = model("AuthorSchema", AuthorSchema);
+
+export default Author;

a8a5f5159cf427d5ab3219b0278e3923f36a9ac7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-19 19:00:11 -0400

    wip: add author types

--- src/models/author.ts
+++ src/models/author.ts
@@ -1,23 +1,31 @@
 import mongoose, { Document, Model } from "mongoose";
 
-export interface IAuthor extends Document {
-  first_name: string;
-  last_name: string;
-}
-
-const AuthorSchema = new mongoose.Schema({
+const AuthorSchema = new mongoose.Schema<AuthorDocument, AuthorModel>({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
 });
 
-AuthorSchema.virtual("name").get(function (this: IAuthor) {
+export interface Author {
+  first_name: string;
+  last_name: string;
+}
+
+export interface AuthorDocument extends Author, Document {
+  name: string;
+  url: string;
+}
+
+AuthorSchema.virtual("name").get(function (this: AuthorDocument) {
   return this.first_name + this.last_name;
 });
 
-AuthorSchema.virtual("url").get(function (this: IAuthor) {
+AuthorSchema.virtual("url").get(function (this: AuthorDocument) {
   return "/api/authors/" + this._id;
 });
 
-const Author: Model<IAuthor> = mongoose.model("Author", AuthorSchema);
+type AuthorModel = Model<AuthorDocument>;
 
-export default Author;
+export default mongoose.model<AuthorDocument, AuthorModel>(
+  "Author",
+  AuthorSchema
+);
--- src/models/book.ts
+++ src/models/book.ts
@@ -1,6 +1,6 @@
 // based on: https://medium.com/@agentwhs/complete-guide-for-typescript-for-mongoose-for-node-js-8cc0a7e470c1
 import mongoose, { Document, Model, Schema, Types } from "mongoose";
-// import { IAuthor } from "./author";
+import { AuthorDocument } from "./author";
 
 const BookSchema = new mongoose.Schema<BookDocument, BookModel>({
   title: { type: String, required: true, maxLength: 100 },
@@ -17,17 +17,17 @@ export interface Book {
   authors: Types.ObjectId | Record<string, unknown>;
 }
 
-// Mongoose Types, not exported because company field is non deterministic
+// Mongoose Types, not exported because author field is non deterministic
 interface BookBaseDocument extends Book, Document {
   url: string;
 }
 
 export interface BookDocument extends BookBaseDocument {
-  author: Author["_id"];
+  author: AuthorDocument["_id"];
 }
 
 export interface BookPopulatedDocument extends BookBaseDocument {
-  author: Author;
+  author: AuthorDocument;
 }
 
 BookSchema.virtual("url").get(function (this: BookBaseDocument) {

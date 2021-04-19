97f1fa72bd97c1625709f17be41e90f6f096d53e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-19 16:55:25 -0400

    wip: improve book types

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,24 +1,39 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
-import { IAuthor } from "./author";
+// based on: https://medium.com/@agentwhs/complete-guide-for-typescript-for-mongoose-for-node-js-8cc0a7e470c1
+import mongoose, { Document, Model, Schema, Types } from "mongoose";
+// import { IAuthor } from "./author";
 
-export interface IBook extends Document {
-  title: string;
-  subtitle?: string;
-  pageCount: number;
-  authors: IAuthor["_id"];
-}
-
-const BookSchema = new mongoose.Schema({
+const BookSchema = new mongoose.Schema<BookDocument, BookModel>({
   title: { type: String, required: true, maxLength: 100 },
   subtitle: { type: String, maxLength: 100 },
   pageCount: { type: Number, required: true, min: 0, max: 32666 },
   authors: [{ type: Schema.Types.ObjectId, ref: "Author", required: true }],
 });
 
-BookSchema.virtual("url").get(function (this: IBook) {
+// ES Types, except for ID
+export interface Book {
+  title: string;
+  subtitle?: string;
+  pageCount: number;
+  authors: Types.ObjectId | Record<string, unknown>;
+}
+
+// Mongoose Types, not exported because company field is non deterministic
+interface BookBaseDocument extends Book, Document {
+  url: string;
+}
+
+export interface BookDocument extends BookBaseDocument {
+  author: Author["_id"];
+}
+
+export interface BookPopulatedDocument extends BookBaseDocument {
+  author: Author;
+}
+
+BookSchema.virtual("url").get(function (this: BookBaseDocument) {
   return "/api/books/" + this._id;
 });
 
-const Book: Model<IBook> = mongoose.model("Book", BookSchema);
+type BookModel = Model<BookDocument>;
 
-export default Book;
+export default mongoose.model<BookDocument, BookModel>("Book", BookSchema);

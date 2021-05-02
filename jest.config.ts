5ae92cbf5b0adbe170f404998b3d80ba4a15e9b5
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-02 16:51:46 -0400

    chore(test): remove mongo preset, restory ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,13 @@
-// import type { Config } from "@jest/types";
 import type { InitialOptionsTsJest } from "ts-jest/dist/types";
-// import { defaults as tsjPreset } from "ts-jest/presets";
-import { jsWithTs as tsjPreset } from "ts-jest/presets";
-// import { jsWithBabel as tsjPreset } from 'ts-jest/presets')
+import { defaults as tsjPreset } from "ts-jest/presets";
 
 const config: InitialOptionsTsJest = {
-  preset: "@shelf/jest-mongodb",
-  // preset: "ts-jest",
+  preset: "ts-jest",
   testEnvironment: "node",
-  // coverageProvider: "v8",
+  coverageProvider: "v8",
   rootDir: "./src",
   roots: ["<rootDir>"],
+  // ts-jest Note: presets use testMatch, like Jest does in its defaults. If you want to use testRegex instead in your configuration, you MUST set testMatch to null or Jest will bail.
   testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
   transform: {
     ...tsjPreset.transform,
@@ -20,7 +17,6 @@ const config: InitialOptionsTsJest = {
   },
   slowTestThreshold: 10,
   // testTimeout: 10000,
-  watchPathIgnorePatterns: ["globalConfig"],
 };
 
 export default config;

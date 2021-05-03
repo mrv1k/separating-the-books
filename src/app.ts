e55d3ef8b522ffc79956acac0861c3aea572d24b
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-02 22:47:55 -0400

    refactor: move express config to express loader
    allows to easily replace mongo for in memory mongo

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,25 @@
 import "dotenv/config";
 
-import cors from "cors";
-import debug from "debug";
-import express, { ErrorRequestHandler } from "express";
-import expressWinston from "express-winston";
-import createError from "http-errors";
-import winston from "winston";
+import express from "express";
 
-import launchMongoDB from "@/loaders/mongoose";
-import authorsRouter from "@/routes/authors";
-import booksRouter from "@/routes/books";
-
-launchMongoDB();
+import expressLoader from "@/loaders/express";
+import mongooseLoader from "@/loaders/mongoose";
 
 const app = express();
+expressLoader(app);
 
-const debugLog = debug("app");
-
-if (process.env.NODE_ENV !== "test") {
-  app.use(
-    expressWinston.logger({
-      transports: [new winston.transports.Console()],
-      format: winston.format.combine(
-        winston.format.json(),
-        winston.format.prettyPrint(),
-        winston.format.colorize()
-      ),
-      meta: false,
-    })
-  );
-}
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/books", booksRouter);
-app.use("/api/authors", authorsRouter);
-
-app.use((req, res, next) => {
-  next(createError(404, "No, can do"));
-});
-
-const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
-  debugLog("Error status:", err.status);
-  debugLog("Error message:", err.message);
-  debugLog("Error stack:", err.stack);
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.json({
-    status: err.status || 500,
-    message: err.message,
-    stack: err.stack?.split("\n"),
-  });
-};
-app.use(errorHandler);
-
-const server = () => {
+const startServer = async () => {
   // fix for: "Jest has detected the following 1 open handle potentially keeping Jest from exiting"
   if (process.env.NODE_ENV === "test") return;
+  await mongooseLoader();
 
   const PORT = process.env.PORT || 3000;
   const server = app.listen(PORT, () => {
-    debugLog(`\nListening on port http://localhost:${PORT}`);
+    console.log(`\nListening on port http://localhost:${PORT}`);
   });
+
   return server;
 };
 
-export default server();
+export default startServer();
 export { app };
---
+++ src/loaders/express.ts
@@ -0,0 +1,54 @@
+import cors from "cors";
+import debug from "debug";
+import express, { Application, ErrorRequestHandler } from "express";
+import expressWinston from "express-winston";
+import createError from "http-errors";
+import winston from "winston";
+
+import authorsRouter from "@/routes/authors";
+import booksRouter from "@/routes/books";
+
+export default (app: Application): Application => {
+  app.use(express.json());
+  app.use(cors());
+
+  app.use("/api/books", booksRouter);
+  app.use("/api/authors", authorsRouter);
+
+  app.use((req, res, next) => {
+    next(createError(404, "No, can do"));
+  });
+
+  const debugLog = debug("app");
+  const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
+    debugLog("Error status:", err.status);
+    debugLog("Error message:", err.message);
+    debugLog("Error stack:", err.stack);
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.json({
+      status: err.status || 500,
+      message: err.message,
+      stack: err.stack?.split("\n"),
+    });
+  };
+  app.use(errorHandler);
+
+  if (process.env.NODE_ENV !== "test") useLogger(app);
+
+  return app;
+};
+
+function useLogger(app: Application) {
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize()
+      ),
+      meta: false,
+    })
+  );
+}

659c3dd0da75b249340c88fa786a8d75a10a724c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-05-06 21:32:14 -0400

    chore(deps): delete mongoose

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,14 +1,5 @@
-import { RequestHandler, RequestParamHandler } from "express";
+import { RequestHandler } from "express";
 import createError from "http-errors";
-import { isValidObjectId } from "mongoose";
-
-export const validateId: RequestParamHandler = (req, res, next, id) => {
-  if (!isValidObjectId(id)) {
-    throw Error("ID is invalid");
-  }
-  res.locals._id = id;
-  next();
-};
 
 export const methodNotAllowed: RequestHandler = (req, res) => {
   res.status(405).json(new createError.MethodNotAllowed());
